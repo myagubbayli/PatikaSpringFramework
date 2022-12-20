acd3c601bf7f0f778a3f80011c0a904ca259180b
Mahammad Yagubbayli <ymahammad@outlook.com>
2022-12-21 01:49:00 +0300

    Hatırlatma

---
+++ src/main/java/com/mahammadyagubbayli/tutorials/JavaTutorials.java
@@ -0,0 +1,68 @@
+package com.mahammadyagubbayli.tutorials;
+
+import java.util.Arrays;
+
+public class JavaTutorials {
+
+    public static void main(String[] args) {
+        // Arrays.sort();
+        // this.
+        // super
+        // break continue return
+        // optional, of, ofnullable, get, ifPresent, isPresent
+        // case
+        // primitive type
+        // wrapper class
+        // Random
+        // Math
+        // Runtime,
+        // Syntax, logical, runtime error structures
+        // %s %d %f ==> printf()
+        // final
+        // static
+        // decoder encoder
+        // instanceof
+        // public private protected
+        // String
+        // StringBuilder
+        // StringTokenizer
+        // StringBuffer
+        // Enum
+        // Multithread
+
+        // S.O.L.I.D
+        // KISS
+        // DRY
+        // MONAD
+        // List Set Map
+        // Liste:LinkedList ArrayList
+        // Generic <>
+        // Iterator
+        // OOP
+        // interface
+        // abstract
+        // inheritance
+
+        // UML - + __ why we use?
+        // class
+        // POJO
+        // Bean
+        // Serialization
+        // IO
+        // JAR WAR
+        // Dağıtık programlama nedir?
+        // Server ve iç bileşenler yapılar
+
+        // Constructor
+        // Builder
+        // Exception
+        // Error
+
+        // java 8 : stream, map, filter, sort
+        // Optional
+        // Lambda Expression
+        // Method Referances ::
+
+        // D.P.
+    }
+}
